3578f83db30201576f34bfe6d2e1dcc2a69c496c
DropPredator <ionchiccc@gmail.com>
2025-09-10 16:56:28 +0300

    task is completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const largeImg = document.querySelector('body .gallery div #largeImg');
+const ulImgs = document.querySelector('body .gallery .gallery__list');
+
+ulImgs.addEventListener('click', (evnt) => {
+  event.preventDefault();
+
+  if (evnt.target.localName !== 'img' && evnt.target.localName !== 'a') {
+    return;
+  }
+
+  if (evnt.target.localName === 'img') {
+    largeImg.setAttribute('src', evnt.target.closest('a').href);
+  } else {
+    largeImg.setAttribute('src', evnt.target.href);
+  }
+});
